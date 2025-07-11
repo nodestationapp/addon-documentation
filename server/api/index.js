e7208e14e7e9703df227e12782d217177a507760
Krystian <krystian.wachnicki5@gmail.com>
2025-07-11 20:14:19 +0700

    chore: add middleware to docs route

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,11 @@
 import getTableDocs from "./getTableDocs.js";
+import auth from "@nstation/auth/utils/authMiddleware.js";
 
 export default [
   {
     method: "GET",
     path: "/tables/:id/docs",
     handler: getTableDocs,
+    middlewares: [auth(["admin"])],
   },
 ];
