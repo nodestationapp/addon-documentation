a333aaa8de12a4e4117df2782f6cb88fe6bd60e2
Krystian <krystian.wachnicki5@gmail.com>
2025-07-19 22:33:04 +0700

    fix: change generator import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import docGenerator from "./server/api/docGenerator/index.js";
+import docGenerator from "./server/api/docGenerate.js";
 
 if (process.argv[2] === "generate") {
   docGenerator({ regenerate: true });
